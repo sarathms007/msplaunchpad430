222e1384ecd21d24db2f5baefaddbeb6ebdc1efc
sarath <root@yamaha-Rx.(none)>
2012-02-18 14:29:47 +0530

    sequential lighting of n leds

---
+++ 4led.c
@@ -0,0 +1,13 @@
+#include<msp430.h>
+//pgm to ligh 4 leds one by one 
+main()
+{int i;
+P1DIR = 0xF;
+P1OUT = 1;
+while(1){
+	for(i = 0; i < 32000; i++);
+	P1OUT = (P1OUT<<1 == 0x10 ? 1 : P1OUT<<1 );
+	}
+}
+
+/*upto 8 leds can be done by changing the value "0x10" in the pgm to "0x41 */
